92c25a67bb2841ec67fe5e689ae9ae5e64bbe46e
GreenRamSilgen <kiranshrs@yahoo.com>
2026-01-05 09:19:10 -0800

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Kiran Shrestha on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
